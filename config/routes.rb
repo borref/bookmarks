129305e0347039d3596dd492f2f03915ebf0e3f2
Andres Castaneda <borref22@gmail.com>
2017-11-05 21:55:33 +0100

    Add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root to: 'bookmarks#index'
   resources :bookmarks
   get 'search', to: 'search#index'
+
+  # API
+
+  namespace :api do
+    resources :bookmarks, except: [:new, :edit]
+  end
 end
