3d0f9fe691dda941a1f0e2b1ef64f76ab865e65d
Andres Castaneda <borref22@gmail.com>
2017-11-05 22:40:33 +0100

    Add actions to BookmarksController

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -1,4 +1,43 @@
 # frozen_string_literal: true
 
 class Api::BookmarksController < ApplicationController
+  before_action :set_bookmark, only: %i[show update destroy]
+
+  def index
+    render json: Bookmark.all
+  end
+
+  def show
+    render json: @bookmark
+  end
+
+  def create
+    bookmark = Bookmark.new(bookmark_params)
+
+    if bookmark.save
+      sign_in bookmark
+      render(json: bookmark, status: :created)
+    else
+      render(json: bookmark.errors, status: :bad_request)
+    end
+  end
+
+  def update
+    current_user.assign_attributes(user_params)
+    if current_user.save
+      render(json: current_user)
+    else
+      render(json: current_user.errors, status: :bad_request)
+    end
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find params[:id]
+  end
+
+  def bookmark_params
+    params.permit(:title, :url, :shortening)
+  end
 end
